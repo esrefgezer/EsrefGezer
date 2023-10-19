204a8822b937be97a5d9a706e6d82b7fa58da9d0
Esref Gezer <esrefgezer72@gmail.com>
2023-10-19 12:13:42 +0200

    merhaba

--- src/murat.java
+++ src/murat.java
@@ -2,5 +2,10 @@ public class murat {
     public static void main(String[] args) {
         System.out.println("Sari renk gozukmuyor hocam");
 
+        int a=300;
+        int b=500;
+        int c=a+b;
+        System.out.println("c="+c);
+
     }
 }
