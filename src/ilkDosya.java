dec2add8baade8352fae5d24f48289ac6e3125ee
Esref Gezer <esrefgezer72@gmail.com>
2023-10-19 12:08:29 +0200

    first commit

---
+++ src/ilkDosya.java
@@ -0,0 +1,15 @@
+public class ilkDosya {
+    public static void main(String[] args) {
+
+        System.out.println("Merhaba gitHub");
+
+        /*
+        git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/esrefgezer/EsrefGezer.git
+git push -u origin main
+         */
+    }
+}
